e5443cdd5ff68278024a497d906a8523a53f5513
Nick Klepinger <bodiddlie@gmail.com>
2019-04-13 17:47:46 -0700

    trying to fix build (#1)
    * trying to fix build
    
    * filtering out contentful posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,7 @@ const { createFilePath } = require('gatsby-source-filesystem');
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
   if (node.internal.type === `Mdx`) {
+    if (node.frontmatter.title === '') return;
     const baseSlug = createFilePath({ node, getNode, basePath: 'posts' });
     let nameArr = baseSlug.replace(/\//g, '').split('-');
     //date = nameArr.splice(0, 3).join("-");
